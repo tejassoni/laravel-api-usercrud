911cbff94dca1c4f558e51351690c5a7f0593923
Tejas Soni <tejas.tejas.soni3@gmail.com>
2023-08-07 12:06:57 +0530

    updated setup

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+
+    use HttpResponses;    
+    public function index(Request $request){
+        $user = User::paginate($request->page ?? 10);
+        return $this->success([
+            'user' => $user,
+        ],"User List Successfully...!");
+    }
+}
